dd4ea2fb0517cd44626160abddd315a3e4140438
yunhanuk <hanugyun@gmail.com>
2024-11-25 23:25:10 +0900

    기본 틀 확장, 화면 초기화 함수 및 종료 함수 선언

--- frame.c
+++ frame.c
@@ -3,17 +3,31 @@
 void OutFrame() {
 	SetColor(8);
 	MoveConsole(20, 0);
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 25; i++) {
 		printf("▒▒");
 	}
-	for (int i = 0; i < 19; i++) {
+	for (int i = 0; i < 24; i++) {
 		MoveConsole(20, i + 1);
 		printf("▒▒");
-		MoveConsole(58, i + 1);
+		MoveConsole(68, i + 1);
 		printf("▒▒");
 	}
-	MoveConsole(20, 19);
-	for (int i = 1; i <= 20; i++) {
+	MoveConsole(20, 24);
+	for (int i = 0; i < 25; i++) {
 		printf("▒▒");
 	}
+}
+
+void ScreenReset() {
+	for (int i = 0; i < 24; i++) {
+		MoveConsole(22, i + 1);
+		for (int i = 1; i <= 46; i++)
+			printf(" ");
+	}
+	OutFrame();
+}
+
+void Finish() {
+	ScreenReset();
+	exit(0);
 }
